98f51d2663e831e642a3a0f6cc1834aa03202576
Dell Vostro <linhphuong021@gmail.com>
2020-07-08 19:15:00 +0700

    kết nối database

--- src/server/ConnectionDB.java
+++ src/server/ConnectionDB.java
@@ -5,10 +5,29 @@
  */
 package server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Dell Vostro
  */
 public class ConnectionDB {
-    
+    public static Connection getConnection(){
+        Connection conn = null;
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectionDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=QLCaFe", "sa", "19101998");
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
 }
